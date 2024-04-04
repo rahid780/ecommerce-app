34415e8ffb879dd5f6d0ef8d1af4ebda479d95ec
apple <apple@192.168.10.4>
2024-04-05 00:38:20 +0500

    8th commit

--- lib/mini_screens/product_details_screen.dart
+++ lib/mini_screens/product_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_provider/main_screens/home_page.dart';
 import 'package:flutter/material.dart';
 
-
 class ProductDetailsScreen extends StatefulWidget {
   final dynamic productDetails;
   const ProductDetailsScreen({super.key, required this.productDetails});
@@ -15,11 +14,13 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orange,
-        leading: IconButton(onPressed: () {
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
-        },
-        icon: const Icon(Icons.arrow_back),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomePage()));
+          },
+          icon: const Icon(Icons.arrow_back),
         ),
         title: Text(widget.productDetails['name']),
       ),
@@ -30,37 +31,75 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.5,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
+            color: Colors.white,
+            child: Card(
+              color: Colors.white,
+              elevation: 5,
+              child: Image(
                 image: NetworkImage(widget.productDetails['image']),
-                fit: BoxFit.cover,
               ),
             ),
           ),
-
           const SizedBox(
             height: 10,
           ),
-
-          Text(
-            widget.productDetails['name'],
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,color: Colors.orange),),
-
-              Text(
-            widget.productDetails['price'].toString(),
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,color: Colors.orange),),
-
-              Text(
-            widget.productDetails['description'],
-            style: const TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.bold,color: Colors.orange),),
-          
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Card(
+              color: Colors.white,
+              elevation: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.productDetails['name'],
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange),
+                            ),
+                            Text(
+                              widget.productDetails['price'].toString(),
+                              style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange),
+                            ),
+                          ],
+                        ),
+                          IconButton(onPressed: (){}, icon: const Icon(Icons.favorite_border, color: Colors.orange, size: 25,))
+                      ],
+                    ),
+                  ),
+                  Text(
+                    widget.productDetails['description'],
+                    style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomSheet: Row(
+        children: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart, color: Colors.black, size: 25,)),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.share, color: Colors.black, size: 25,)),
+          Container(
+            color: Colors.orange,
+            child: TextButton(onPressed: (){}, child: const Text('Add to Cart', style: TextStyle(color: Colors.black,fontSize: 14),)))
         ],
       ),
     );
